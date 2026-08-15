4eba9aa416d6f7140a11afb9661ffc88142ce730
DilayGobel <2023141012@cumhuriyet.edu.tr>
2026-08-15 21:03:30 +0300

    Update dio_client.dart

--- Flutter-Blog/gelecege_mektuplar/lib/core/network/dio_client.dart
+++ Flutter-Blog/gelecege_mektuplar/lib/core/network/dio_client.dart
@@ -9,8 +9,9 @@ final dioProvider = Provider<Dio>((ref) {
   final dio = Dio(
     BaseOptions(
       baseUrl: ApiConstants.baseUrl,
-      connectTimeout: const Duration(seconds: 10),
-      receiveTimeout: const Duration(seconds: 10),
+      connectTimeout: const Duration(seconds: 60),
+      receiveTimeout: const Duration(seconds: 60),
+      contentType: 'application/json',
     ),
   );
 
